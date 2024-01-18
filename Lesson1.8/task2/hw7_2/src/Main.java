aa1aded6c5cfe8b7c4bcbdc8e9af834ddabc5d93
ALEH BIAHUN <stones2875@gmail.com>
2024-01-18 19:17:48 +0100

    add hm7_2 (18_01_24) version 1.1

--- Lesson1.8/task2/hw7_2/src/Main.java
+++ Lesson1.8/task2/hw7_2/src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
         //todo Тут увольняем айтишников
-        int [] numberOfItSpecialists = new int[100];
 
-        int numberId;
+        int[]numberOfItSpecialists = new int[100];
 
-        while (int i%2 = 0) {
-            System.out.println("Уволен айтишник с id" + numberId );
+        for (int i = 2; i <= numberOfItSpecialists.length; i = i+2) {
+            if (i%2 == 0)
+            System.out.println("Уволен айтишник с id " + i );
         }
     }
 }
