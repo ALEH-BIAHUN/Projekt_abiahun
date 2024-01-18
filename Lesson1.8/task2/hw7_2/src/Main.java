8de941ef67f3c489c483f860bf2f1ce4332e90b9
ALEH BIAHUN <stones2875@gmail.com>
2024-01-18 18:29:59 +0100

    add hw7_2 (18.01.24) version_1

--- Lesson1.8/task2/hw7_2/src/Main.java
+++ Lesson1.8/task2/hw7_2/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int [] numberOfItSpecialists = new int[100];
+
+        int numberId;
+
+        while (int i%2 = 0) {
+            System.out.println("Уволен айтишник с id" + numberId );
+        }
     }
 }
